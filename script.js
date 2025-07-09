50a2afe6ee8305b68437c4548624480ee37c16d9
Ayush Narula <132435932+Ayush-Narula-100@users.noreply.github.com>
2025-07-09 14:20:41 +0530

    finale

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+(function () {
+  emailjs.init("ILuQ6IdFvsmUNMJpD"); // Replace with your EmailJS user/public key
+})();
+
+document.getElementById("contact-form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  emailjs.sendForm("service_1bm3nar", "template_4ljwkbs", this).then(
+    function () {
+      alert("Message sent successfully!");
+      document.getElementById("contact-form").reset();
+    },
+    function (error) {
+      alert("Failed to send message. Please try again later.");
+      console.log(error);
+    }
+  );
+});
